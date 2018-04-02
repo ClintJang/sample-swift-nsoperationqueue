ba0f66734ba1512ba2a7eee1761e75994909bae8
Clint Jang <jang.wangsu@gmail.com>
2018-04-02 22:24:49 +0900

    modify

--- JWSOperationQueueSample/JWSOperationQueueSample/OperationQueueManager.swift
+++ JWSOperationQueueSample/JWSOperationQueueSample/OperationQueueManager.swift
@@ -8,4 +8,48 @@
 
 import Foundation
 
-
+final class OperationQueueManager {
+    static let shared = OperationQueueManager()
+    
+    init() {
+        
+        // hm.......
+        thread.name = "runLoop"
+//        thread.qualityOfService = .background
+//        thread.perform(Selector(("runLoop")))
+//        thread.perform(Selector("runLoop"))
+        thread.perform(Selector(("runLoop")), with: nil, afterDelay: 0.1)
+        
+        //thread
+        thread.start()
+    }
+    
+//    private var queue:OperationQueue = {
+//        return OperationQueue()
+//    }()
+    
+    private var thread:Thread = {
+        var object = Thread()
+        //loop.qualityOfService = .default
+        
+        return object
+    }()
+    
+    private func runLoop() {
+        
+        // why?
+        assert(Thread.isMainThread == false)
+        assert(Thread.isMainThread == true)
+//
+//        print(#function)
+//        while true {
+//            autoreleasepool {
+//                RunLoop.current.run()
+//            }
+//        }
+    }
+    
+//    override func addObserver(_ observer: NSObject, forKeyPath keyPath: String, options: NSKeyValueObservingOptions = [], context: UnsafeMutableRawPointer?) {
+//
+//    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sample-swift-nsoperationqueue
-I'm studying about NSOperationQueue. The content is very poor. I am trying to create a sample. I am doing it.
+I'm studying about NSOperationQueue. The content is very poor. I am trying to create a sample.
+
+- https://magi82.github.io/gcd-01/
+- Let's use GCD! This is just a try.
+
